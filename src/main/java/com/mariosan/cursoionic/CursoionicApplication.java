071c9bd89ae65e53a9bfd7a926b739a123032f84
Mariosan Pereira <ti.junior.22@gmail.com>
2020-05-26 18:42:42 -0300

    Projeto criado

---
+++ src/main/java/com/mariosan/cursoionic/CursoionicApplication.java
@@ -0,0 +1,13 @@
+package com.mariosan.cursoionic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoionicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoionicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mariosan/cursoionic/CursoionicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mariosan.cursoionic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoionicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
